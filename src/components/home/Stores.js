89318900382ff08a9a29fed590dda3b0a6bd7381
Onugwu Chideraa <chideraabernard@gmail.com>
2023-03-05 03:21:46 +0100

    Responsiveness modified

--- src/components/home/Stores.js
+++ src/components/home/Stores.js
@@ -4,9 +4,12 @@ import google_logo from '../assets/google_play.svg';
 
 export const Stores = () => {
   return (
-    <div className="download">
-        <img className='AppStore' src={apple_logo} alt='AppStore_logo' />
-        <img className='GooglePlay' src={google_logo} alt='GooglePlay_logo' />
-    </div>
+    <main className='download_main'>
+        <div className="download">
+            <img className='AppStore' src={apple_logo} alt='AppStore_logo' />
+            <img className='GooglePlay' src={google_logo} alt='GooglePlay_logo' />
+        </div>
+    </main>
+    
   )
 }
